b8c882de5a8938f99f20ebb46ec34fffec5cb2bd
Stichoza <stichoza@gmail.com>
2013-06-02 15:47:25 +0400

    Initial

---
+++ projectswitch.bat
@@ -0,0 +1,27 @@
+@ECHO off
+:STARTPOINT
+IF NOT EXIST webx (
+    MKDIR webx
+	IF EXIST www (
+		MOVE www webx
+		MKLINK /D /H /J www webx\www
+	)
+	CLS
+)
+:INTRO
+echo ProjectSwitch by Stichoza [me@stichoza.com]
+@echo.
+:ENTERNAME
+SET /P PROJECTNAME=Enter directory name: 
+@RMDIR www
+IF NOT EXIST webx\%PROJECTNAME% (
+	CD webx
+	MKDIR %PROJECTNAME%
+	CD ..
+)
+MKLINK /D /H /J www webx\%PROJECTNAME%
+ECHO Press any key to exit.
+PAUSE>nul
+GOTO QUIT
+:QUIT
+EXIT
